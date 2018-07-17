cac0604bcce3b15a6c50fd767173fe19c32e2a0d
Reayah Bravo <reayahbravo@gmail.com>
2018-07-16 20:10:54 -0700

    Getting ready to push

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+        # render json: parms
+    end
+
+    def edit
+    end
+
+    def index
+    end
+    
+    def create
+#     #render json: params
+    @user = User.new user_params
+
+    if @user.save
+
+    session[:user_id] = @user.id
+    redirect_to home_path
+    
+    else
+        render :new
+    end
+    end
+    
+      private
+      def user_params
+        params.require(:user).permit(
+          :first_name, :last_name, :email, :password, :password_confirmation
+        )
+      end
+  end
---
+++ spec/views/ideas/—no_assets.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "ideas/—no_assets.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
